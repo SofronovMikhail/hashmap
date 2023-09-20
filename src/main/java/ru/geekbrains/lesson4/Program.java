0063ea566e727a91630d5e0b14c56adcc92fa080
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-09-21 02:28:51 +0300

    first commit

---
+++ src/main/java/ru/geekbrains/lesson4/Program.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.lesson4;
+
+import java.util.ArrayList;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> hashMap = new HashMap<>(4);
+        String oldValue = hashMap.put("+79001112233", "AAAAAAAA3");
+        oldValue = hashMap.put("+79001112231", "BBBBBB");
+        oldValue = hashMap.put("+79001112232", "CCCCCC");
+        oldValue = hashMap.put("+79001112233", "DDDDDDDD");
+        oldValue = hashMap.put("+79001112234", "EEEEEEE");
+        oldValue = hashMap.put("+79001112235", "MMMMMM");
+        oldValue = hashMap.put("+79001112236", "FFFFF");
+        oldValue = hashMap.put("+79001112237", "GGGGG1");
+        oldValue = hashMap.put("+79001112238", "GGGGG2");
+        oldValue = hashMap.put("+79001112239", "GGGGG3");
+      
+        hashMap.Print();
+        
+
+
+    }
+
+}
